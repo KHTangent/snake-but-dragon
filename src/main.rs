b5ffb64e7796fcb841ecbc99cbb88ec00d33f5f2
KHTangent <contact@khtangent.com>
2025-11-11 16:46:09 +0100

    Implement food eating

--- src/main.rs
+++ src/main.rs
@@ -116,6 +116,7 @@ fn make_segment() -> impl Bundle {
 				y: GRID_CONTENTS,
 				z: 1.0,
 			},
+			translation: Vec3::new(-WINDOW_SIZE.x, -WINDOW_SIZE.y, 1.0),
 			..default()
 		},
 	)
@@ -145,6 +146,7 @@ fn make_food(player_pos: &Vec2, player: &Player) -> impl Bundle {
 				y: GRID_CONTENTS,
 				z: 1.0,
 			},
+			translation: Vec3::new(-WINDOW_SIZE.x, -WINDOW_SIZE.y, 1.0),
 			..default()
 		},
 	)
@@ -182,15 +184,25 @@ fn process_tick(time: Res<Time>, mut tick_timer: ResMut<TickTimer>) {
 	tick_timer.timer.tick(elapsed);
 }
 
-fn move_player(tick_timer: Res<TickTimer>, player_query: Single<(&mut GridPos, &mut Player)>) {
+fn move_player(
+	tick_timer: Res<TickTimer>,
+	player_query: Single<(&mut GridPos, &mut Player)>,
+	mut eated_events: ResMut<Events<FoodEated>>,
+) {
 	if !tick_timer.timer.finished() {
 		return;
+	}
+	let mut just_ate = false;
+	for _ in eated_events.drain() {
+		just_ate = true;
 	}
 	let (mut player_pos, mut player) = player_query.into_inner();
 	player.facing = player.next_movement.clone();
 	player.segment_positions.push_back(player_pos.0.clone());
 	player_pos.0 += player.facing.to_vec2();
-	player.segment_positions.pop_front();
+	if !just_ate {
+		player.segment_positions.pop_front();
+	}
 }
 
 fn move_segments(
@@ -202,9 +214,10 @@ fn move_segments(
 		return;
 	}
 	for (index, mut segment_pos) in segments.into_iter().enumerate() {
-		let pos = player.segment_positions.get(index).unwrap();
-		segment_pos.0.x = pos.x;
-		segment_pos.0.y = pos.y;
+		if let Some(pos) = player.segment_positions.get(index) {
+			segment_pos.0.x = pos.x;
+			segment_pos.0.y = pos.y;
+		}
 	}
 }
 
@@ -222,6 +235,21 @@ fn check_self_intersect(
 			next_state.set(GameStates::GameOver);
 		}
 	}
+}
+
+fn handle_food_eating(
+	player_pos: Single<&GridPos, With<Player>>,
+	food: Single<(&GridPos, Entity), With<Food>>,
+	mut commands: Commands,
+	mut food_eated_writer: EventWriter<FoodEated>,
+) {
+	let (food_pos, food_entity) = food.into_inner();
+	if player_pos.0 != food_pos.0 {
+		return;
+	}
+	commands.entity(food_entity).despawn();
+	commands.spawn(make_segment());
+	food_eated_writer.write(FoodEated);
 }
 
 fn spawn_food_if_needed(
@@ -275,7 +303,7 @@ fn main() {
 			}),
 			..default()
 		}))
-		.add_event::<FoodEated>()
+		.init_resource::<Events<FoodEated>>()
 		.add_systems(Startup, setup)
 		.add_systems(
 			FixedUpdate,
@@ -285,8 +313,9 @@ fn main() {
 					process_tick,
 					((
 						move_player,
+						handle_food_eating,
 						move_segments,
-						check_self_intersect,
+						(check_self_intersect, handle_food_eating),
 						spawn_food_if_needed,
 					)
 						.chain()
